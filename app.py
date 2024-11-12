206c2338a64e414e29c86c69c883d29f5c9f0b39
sherry <95038227+swiftiecoder@users.noreply.github.com>
2024-11-12 11:58:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+GOOGLE_API_KEY = os.environ.get('GEMINI_API_KEY')
+
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return f"{TELEGRAM_BOT_TOKEN} and {GOOGLE_API_KEY}"
 
 if __name__ == '__main__':
     app.run()
